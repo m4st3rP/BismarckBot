b286ac7a3ffd161f17e6e7a13aea83a295a736a0
Philipp Schwarz <schwarz_philipp@hotmail.de>
2019-03-06 20:25:42 +0100

    bot works now almost

--- BismarckBot.py
+++ BismarckBot.py
@@ -1,35 +1,26 @@
+import praw
 import discord
 import asyncio
-from RedditScraper import RedditScraper
 
 
 class BismarckBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rs = RedditScraper("animemes")
-        self.channel_id = open("channel.txt", "r").readline()
-        self.token = open("token.txt", "r").readline()
-        self.bg_task = self.loop.create_task(self.post_subreddit_images())  # create the background task and run it in the background
 
-    async def post_subreddit_images(self):
-        await self.wait_until_ready()
-        channel = self.get_channel(self.channel_id)
-        while not self.is_closed:
-            print("Ich hol mir mal paar Bilder")
-            posts = await self.rs.get_posts(3000)
-            print("Bilder geholt")
-            for post in posts:
-                await channel.send(post)
-                print("Ein Bild gepostet")
-                await asyncio.sleep(3)
-            print("Fertig mit Bilder posten")
-            await asyncio.sleep(1800)  # task runs every 30 minutes
+        self.channel_id = open("channel.txt", "r").readline()
+        # create the background task and run it in the background
+        self.bg_task = self.loop.create_task(self.post_subreddit_images("animemes", 3000, self.channel_id))
+        self.done_posts = []
+        self.token = open("token.txt", "r").readline()
 
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
         print('------')
+        with open("done_posts.txt", "r") as f:
+            for line in f:
+                self.done_posts.append(line.strip())
 
     async def on_message(self, message):
         if message.author == client.user:
@@ -37,6 +28,32 @@ class BismarckBot(discord.Client):
 
         if message.content.startswith('!hallo'):
             await message.channel.send('Hallo!')
+
+    async def post_subreddit_images(self, subreddit_name, min_score, channel_id):
+        await self.wait_until_ready()
+        channel = self.get_channel(channel_id)  # channel ID goes here
+        while not self.is_closed():
+            posts = await self.get_posts(subreddit_name, min_score)
+            for post in posts:
+                await channel.send(post)
+                await asyncio.sleep(3)
+            await asyncio.sleep(1800)  # task runs every 30 min
+
+    async def get_posts(self, subreddit_name, min_score):
+        reddit = praw.Reddit("bot1")
+        subreddit = reddit.subreddit(subreddit_name)
+
+        links = []
+        for submission in subreddit.hot(limit=25):
+            await asyncio.sleep(2)
+            if submission.score >= min_score:
+                post_url = submission.url
+                if post_url not in self.done_posts:
+                    links.append(post_url)
+                    self.done_posts.append(post_url)
+                    with open("done_posts.txt", "a") as f:
+                        f.write(post_url + "\n")
+        return links
 
 
 client = BismarckBot()
